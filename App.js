e216fb9c32a22cddfbdaa700863419255aa382ee
Vivek Kumar <vivekkumar06102002@gmail.com>
2024-06-11 02:53:53 +0530

    third changes

---
+++ App.js
@@ -0,0 +1,6 @@
+const inputBox = document.querySelector('.input-box');
+const searchBtn = document.querySelector('.searchBtn');
+const weather_img = document.querySelector('.weather_img');
+const description = document.getElementById('.description');
+const humidity = document.getElementryById('.humidity');
+const wind = document.getElementById('.wind');
